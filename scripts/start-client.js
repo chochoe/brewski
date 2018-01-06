a0d3ee071fb1e910e1416bfca4edb6f471780836
Emery Cho <cho.e@live.com>
2018-01-05 20:53:06 -0800

    Update start-client.js

--- scripts/start-client.js
+++ scripts/start-client.js
@@ -1,3 +1,5 @@
+// a file needed to get the app running
+
 const args = ["start"];
 const opts = { stdio: "inherit", cwd: "client", shell: true };
 require("child_process").spawn("npm", args, opts);
